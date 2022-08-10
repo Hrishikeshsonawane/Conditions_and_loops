e0a088adfb1d8ef8267e0975e54bda41a19a3913
Hrishikeshsonawane <hrishikeshsonawane9@gmail.com>
2022-08-10 13:45:21 +0530

    hello

---
+++ src/Com.Ashu/cond.java
@@ -0,0 +1,17 @@
+package Com.Ashu;
+
+public class cond {
+    public static void main(String[] args) {
+
+ int Salary = 24500;
+ if(Salary> 10000){
+     Salary += 5500;
+ }
+ else{
+     Salary += 4000;
+ }
+        System.out.println(Salary);
+    }
+
+
+}
---
+++ src/Com.Ashu/loops.java
@@ -0,0 +1,23 @@
+package Com.Ashu;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args) {
+ // For loop
+        //Q-1 print from 1 to 5
+//        for(int num =1; num <=5; num += 1){
+//            System.out.println(num);
+//        }
+         //Q-2 print from 1 to n
+//
+
+        // While loop
+
+        int num = 1;
+        while(num <=5){
+            System.out.println(num);
+            num++;
+        }
+    }
+}
